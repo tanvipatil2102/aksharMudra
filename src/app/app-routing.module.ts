50678a4b6a545d02d33e887ec03ae8ca9ad6d1db
tanvipatil2102 <tanvi6829@gmail.com>
2025-05-12 21:13:57 +0530

    work on aksharmudra website

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './shared/components/home/home.component';
+import { DetailsComponent } from './shared/components/details/details.component';
+import { DashboardComponent } from './shared/components/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : '',
+    component : HomeComponent
+  },
+  {
+    path : 'home',
+    component : HomeComponent
+  },
+  {
+    path : 'details',
+    component : DashboardComponent
+  },
+  {
+    path : 'about',
+    component : DetailsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardComponent } from './shared/components/dashboard/dashboard.component';
+import { HomeComponent } from './shared/components/home/home.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { MaterialModule } from './material/material/material.module';
+import { DetailsComponent } from './shared/components/details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HomeComponent,
+    NavbarComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
